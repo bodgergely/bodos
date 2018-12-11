62f0f8d1d5c6b4a74b8cde16b079b823606afe9e
Gergely Bod <bodgergely@hotmail.com>
2018-12-11 14:33:28 +0000

    Use green text

--- src/kernel/arch/i386/tty.c
+++ src/kernel/arch/i386/tty.c
@@ -7,9 +7,6 @@
 #include "cursor.h"
 
 
-
-
-
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
@@ -18,7 +15,7 @@ uint16_t* terminal_buffer;
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
 	terminal_buffer = vga_buffer_address;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
